616e4c75242b2afb6d5f3edcbb0114176f051ce0
Griff00 <50583567+Griff00@users.noreply.github.com>
2021-03-23 14:51:57 +0000

    Added DAC Interrupt vector

--- main.s
+++ main.s
@@ -1,10 +1,13 @@
 #include <xc.inc>
-extrn NC_Run_Game, Keypad_Setup, Keypad_Read
+extrn NC_Run_Game, Keypad_Setup, Keypad_Read, DAC_Int_Hi
 
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	start
-
+	
+;When interupt flagged, this is branched to
+int_hi:	org	0x0008	; high vector, no low vector
+	goto	DAC_Int_Hi
 
 	
 start:	;movlw	0x00
