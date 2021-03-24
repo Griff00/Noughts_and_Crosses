c9753cda51c65912ca388fb9dab38fed1c65b54c
georg <georg@DESKTOP-S01LCS9>
2021-03-24 15:04:24 +0000

    Added tune

--- main.s
+++ main.s
@@ -1,16 +1,13 @@
 #include <xc.inc>
 extrn NC_Run_Game, Keypad_Setup, Keypad_Read
-extrn	DAC_Setup, DAC_Int_Hi
+extrn	DAC_Int_Hi
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	start
 
 int_high: org 0x0008 ;the place to go when interrupts happen
-	call DAC_Int_Hi
+	goto DAC_Int_Hi
 start:	
-	call Keypad_Setup
-	call DAC_Setup
-	goto $ 
 	
     game_loop:
 	call NC_Run_Game
