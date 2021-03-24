6049be0c183f457c612af387908fd04bb766dd87
georg <georg@DESKTOP-S01LCS9>
2021-03-24 10:36:30 +0000

    Introduced DAC file

--- main.s
+++ main.s
@@ -1,17 +1,17 @@
 #include <xc.inc>
 extrn NC_Run_Game, Keypad_Setup, Keypad_Read
-
+extrn	DAC_Setup, DAC_Int_Hi
 psect	code, abs
 rst:	org	0x0000	; reset vector
 	goto	start
 
-
-	
-start:	;movlw	0x00
-;	movwf	TRISD, A
-;	movlw	0xff
-;	movwf	LATD, A
+int_high: org 0x0008 ;the place to go when interrupts happen
+	call DAC_Int_Hi
+start:	
 	call Keypad_Setup
+	call DAC_Setup
+	goto $ 
+	
     game_loop:
 	call NC_Run_Game
 	
